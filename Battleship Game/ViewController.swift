61865741082c644ad5b5c9df9eb6565ff370e1ce
Natan Portilho <natanportilho@outlook.com>
2016-03-20 17:55:00 -0400

    Initial Commit

---
+++ Battleship Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Battleship Game
+//
+//  Created by Natan Portilho on 3/20/16.
+//  Copyright © 2016 Natan Portilho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
